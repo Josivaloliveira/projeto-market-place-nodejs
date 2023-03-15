69b630fbcc934053276427c25b4c3055155d690a
José Josival Bernardo de Oliveira <jj.oliveir@bol.com.br>
2023-03-15 15:53:12 -0300

    Feat: Validação no usuario, categoria e produto

--- src/router/categoria.router.js
+++ src/router/categoria.router.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const authMiddleware = require("../middleware/auth.middleware");
 
 const categoriaController = require("../controller/categoria.controller");
+const { validacategoria } = require("../middleware//validacao.middleware");
 
 router.get("/find/:id", authMiddleware, categoriaController.findCategoriaByIdController);
 router.get("/findAll", authMiddleware, categoriaController.findAllCategoriaController);
 
-router.post("/create", authMiddleware, categoriaController.createCategoriaController);
+router.post("/create", authMiddleware, validacategoria, categoriaController.createCategoriaController);
 
-router.put("/update/:id", authMiddleware, categoriaController.updateCategoriaController);
+router.put("/update/:id", authMiddleware, validacategoria, categoriaController.updateCategoriaController);
 
 router.delete("/delete/:id", authMiddleware, categoriaController.deleteCategoriaController);
 
